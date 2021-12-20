881020d28c1444acb747f5f3d3d507456d010b53
Vu Nguyen <aqaurius6666@gmail.com>
2021-12-20 20:03:45 +0700

    feat: new seed

--- src/internal/db/cockroach/user/user.cdb.seed.go
+++ src/internal/db/cockroach/user/user.cdb.seed.go
@@ -2,7 +2,8 @@ package user
 
 var SQL = `
 insert into public.users (id, updated_at, created_at, deleted_at, username, hash_password, use_default_password, role_id, admin_div_id, is_active)
-values  ('27e99822-65b0-4b97-9062-8d5f3ca7a6b7', 1639926636491, 1639926636491, 0, 'citizen0101', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, '89c913de-9254-4e8c-a09b-492d5a248f28', '5e3363c6-0178-4fa9-a71c-1af2f9c0708d', true),
+values  ('09944b70-a52e-4f38-b856-d7a0d987e8ce', 1640002866414, 1640002866414, 0, 'citizen01010101', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, 'f508a982-2016-45e5-a429-ba1bd229dca4', '33f12b81-fa28-4a47-8beb-eb1ee352b89f', true),
+        ('27e99822-65b0-4b97-9062-8d5f3ca7a6b7', 1639926636491, 1639926636491, 0, 'citizen0101', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, '89c913de-9254-4e8c-a09b-492d5a248f28', '5e3363c6-0178-4fa9-a71c-1af2f9c0708d', true),
         ('622924ba-ec91-405f-aa61-851a295bb678', 1639926538564, 1639926538564, 0, 'citizen03', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, '68a264e2-4f1b-42a5-a0ff-5c4ea50ef15f', 'b996248f-7fa8-44e1-b695-4eb8b821ec6d', true),
         ('65066f74-4a1a-4d6b-bfa1-1bee6f494060', 1639926483693, 1639926483693, 0, 'citizen01', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, '68a264e2-4f1b-42a5-a0ff-5c4ea50ef15f', 'fbe83e5b-4416-4caa-96c4-2b825e687415', true),
         ('7c0a6259-b82a-4c29-a7a3-d18d6b2a4b87', 1639926692528, 1639926692528, 0, 'citizen010101', 'Ep1wn1V67h4ytvjI6ZCf7wcIrEf8rjfAmKAGdcfCfSU=', false, '3157db78-b283-4a23-8b73-669b27be17fa', 'f5ccb72f-12bb-4b91-8b75-946696fd3322', true),
